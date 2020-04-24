61c5c777a9305e2d4d35aaaf11b96da2257da29f
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-04-24 19:47:26 -0400

    Done.

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -5,16 +5,83 @@ import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
+
+  state = {
+    pokemon: [],
+    search: '',
+    pokemonLoaded: false,
+  }
+
+  constructor() {
+    super();
+    console.log('constructed');
+  }
+
+  componentDidMount() {
+    console.log('mounted');
+    this.fetchPokemon();
+  }
+
+  fetchPokemon() {
+    console.log('fetching...')
+    // fetch the pokemon list
+    fetch('http://localhost:3000/pokemon')
+      .then(r => r.json())
+      .then(pokemon => this.setState({ pokemon: pokemon, pokemonLoaded: true }));
+  }
+
+  filteredPokemon = () => {
+    console.log('filtering...')
+
+    let filteredPokemon = this.state.pokemon
+    
+    if (this.state.search.length > 0) {
+      filteredPokemon = filteredPokemon.filter(pokemon => pokemon.name.toLowerCase().indexOf(this.state.search.toLowerCase()) >= 0);
+    }
+
+    return filteredPokemon;
+  }
+
+  handleFilterChange = (event) => {
+    this.setState({ search: event.target.value });
+  }
+
+  addPokemon = (pokemon) => {
+    fetch('http://localhost:3000/pokemon',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(pokemon),
+    })
+      .then(r => r.json())
+      .then(newPokemon => {
+        // console.log(newPokemon);
+        // debugger;
+        this.setState({ pokemon: this.state.pokemon.concat([newPokemon]) });
+      });
+  }
+
   render() {
+
+    console.log('pokemonpage rendering...')
+
     return (
       <Container>
+        <br />
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm addPokemon={this.addPokemon} />
         <br />
-        <Search onChange={() => console.log('🤔')} />
+        <Search onChange={this.handleFilterChange} searchText={this.state.search} />
         <br />
-        <PokemonCollection />
+        {
+          this.state.pokemonLoaded 
+          ?
+          <PokemonCollection pokemon={this.filteredPokemon()} />
+          :
+          <h2>Loading...</h2>
+        }
       </Container>
     )
   }
